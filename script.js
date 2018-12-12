1a24d5d26561c7b95ad89130ea9948538ac7ccb4
“Ime <stefan.njegomirovic@gmail.com>
2018-12-12 14:56:25 +0100

    card matching done

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 const cards = document.querySelectorAll(".card");
 var flips = 0;
-function flipCard(){
-  this.classList.toggle("flip");
+
+var hasFlippedCard = false;
+var firstCard, secondCard;
+
+function flipCard() {
+  this.classList.add("flip");
   flips++;
   document.getElementById("num-of-flips").innerHTML = flips;
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    hasFlippedCard = false;
+    secondCard = this;
+  }
+  checkForMatch();
+
+}
+function checkForMatch(){
+  if (firstCard.id == secondCard.id) {
+    disableCards();
+  }else setTimeout(()=>{
+    unflipCards();
+  },1500);
+}
+function disableCards(){
+  firstCard.removeEventListener('click',flipCard);
+  secondCard.removeEventListener('click',flipCard);
+}
+function unflipCards(){
+  firstCard.classList.remove('flip');
+  secondCard.classList.remove('flip');
 }
 
 for (var i = 0; i < cards.length; i++) {
-  cards[i].addEventListener('click',flipCard);
+  cards[i].addEventListener('click', flipCard);
 }
