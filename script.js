e20d8c76c12ba18e7f9e93809b8a3c568867a797
“Ime <stefan.njegomirovic@gmail.com>
2018-12-12 16:13:40 +0100

    fixed bug in matching

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var flips = 0;
 var hasFlippedCard = false;
 var firstCard, secondCard;
 
+
 function flipCard() {
   this.classList.add("flip");
   flips++;
@@ -12,27 +13,51 @@ function flipCard() {
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
+    firstCard.removeEventListener('click', flipCard);
+    return;
   } else {
     hasFlippedCard = false;
     secondCard = this;
+    secondCard.removeEventListener('click', flipCard);
+    checkForMatch(firstCard, secondCard);
   }
-  checkForMatch();
+
+  console.log(firstCard.id + " " + secondCard.id);
+}
+
+function checkForMatch(firstCard, secondCard) {
+  if (firstCard.id === secondCard.id) {
+    disableCards(firstCard, secondCard);
+  } else{
+    setTimeout(() => {
+      unflipCards(firstCard, secondCard);
+      firstCard.addEventListener('click', flipCard);
+      secondCard.addEventListener('click', flipCard);
+    }, 1500);
+
+  }
 
 }
-function checkForMatch(){
-  if (firstCard.id == secondCard.id) {
-    disableCards();
-  }else setTimeout(()=>{
-    unflipCards();
-  },1500);
+
+function disableCards(firstCard, secondCard) {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
 }
-function disableCards(){
-  firstCard.removeEventListener('click',flipCard);
-  secondCard.removeEventListener('click',flipCard);
-}
-function unflipCards(){
+
+function unflipCards(firstCard, secondCard) {
   firstCard.classList.remove('flip');
   secondCard.classList.remove('flip');
+}
+
+function countdown() {
+  var timer = setInterval(function() {
+
+    time = document.getElementById("time-remaining").innerHTML--;
+
+    if (time <= 0) {
+      clearInterval(timer);
+    }
+  }, 1000);
 }
 
 for (var i = 0; i < cards.length; i++) {
